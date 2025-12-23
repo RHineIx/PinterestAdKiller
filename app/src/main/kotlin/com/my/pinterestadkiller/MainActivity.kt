e3cae1d5f67a0997edf43662bca04698c0fd497d
RHineIx <bakr300max@gmail.com>
2025-12-23 20:46:32 +0300

    Finalize module: Fix size, hide icon, add scope

--- app/src/main/kotlin/com/my/pinterestadkiller/MainActivity.kt
+++ app/src/main/kotlin/com/my/pinterestadkiller/MainActivity.kt
@@ -1,25 +1,19 @@
 package com.my.pinterestadkiller
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.my.pinterestadkiller.databinding.ActivityMainBinding
+import android.view.Gravity
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
-    
-    private var _binding: ActivityMainBinding? = null
-    private val binding: ActivityMainBinding
-        get() = checkNotNull(_binding) { "Activity has been destroyed" }
-    
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
         
-        binding.textView.text = "Hello, Basic Activity!"
-    }
-    
-    override fun onDestroy() {
-        super.onDestroy()
-        _binding = null
+        val textView = TextView(this)
+        textView.text = "Pinterest AdKiller Active"
+        textView.textSize = 20f
+        textView.gravity = Gravity.CENTER
+        
+        setContentView(textView)
     }
 }
